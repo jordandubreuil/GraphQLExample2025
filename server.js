882699edb465854a6971497d9ea92efd8f1216e2
jordandubreuil <jdubreuil@neit.edu>
2025-02-26 21:14:17 -0500

    End of Class Week 8

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const {graphqlHTTP} = require("express-graphql");
 const {buildSchema} = require("graphql");
+const mongoose = require("mongoose");
 
-let users = [
-    {id:"1",name:"Jordan", email:"jdubreuil@neit.edu"},
-    {id:"2",name:"Andrew", email:"andrew@neit.edu"},
-    {id:"3",name:"Ian", email:"ian@neit.edu"},
-];
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+const db = mongoose.connection;
+db.once("open", ()=>console.log("Connected to MongoDB"));
+
+//Mongoose Schema for users
+const UserSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number
+})
+
+const User = mongoose.model("User", UserSchema)
+// let users = [
+//     {id:"1",name:"Jordan", email:"jdubreuil@neit.edu"},
+//     {id:"2",name:"Andrew", email:"andrew@neit.edu"},
+//     {id:"3",name:"Ian", email:"ian@neit.edu"},
+// ];
 
 
 //Define the build Schema
@@ -15,6 +31,7 @@ const schema = buildSchema(`
         id:ID!
         name:String!
         email:String!
+        age:Int!
     }
     
     type Query{
@@ -23,18 +40,26 @@ const schema = buildSchema(`
     }
 
     type Mutation{
-        addUser(name: String!, email:String!):User
+        addUser(name: String!, email:String!, age:Int!):User
+        updateUser(id:ID!, name:String, email:String, age:Int):User
+        deleteUser(id:ID!):String
     }
 `);
 
 //Define Resolvers
 const root = {
-    user:({id})=> users.find(user => user.id == id),
-    users:()=>users,
-    addUser:({name,email})=>{
-        const newUser = {id:String(users.length + 1), name, email};
-        users.push(newUser);
-        return newUser;
+    user:async({id})=> await User.findById(id),
+    users:async()=> await User.find(),
+    addUser:async({name,email,age})=>{
+        const newUser = new User({name, email, age});
+        return await newUser.save();
+    },
+    updateUser:async({id, name,email,age}) =>{
+        return await User.findByIdAndUpdate(id, {name, email,age}, {new:true});
+    },
+    deleteUser:async({id})=>{
+        await User.findByIdAndDelete(id);
+        return `User with ID ${id} deleted`;
     }
 }
 
